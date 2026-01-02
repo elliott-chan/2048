f7fd9c589c03b0b5a0adbb2d4f16806a020908c3
elliott-chan <elliottgregorychan@gmail.com>
2026-01-02 14:42:15 -0500

    Main syntax fix

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from tkinter import Tk
-from gui import DCMApp
+from gui import App
 
 def main():
     root = Tk()
     root.title("2048")
     root.geometry("900x640")
     root.minsize(800, 560)
-    app = DCMApp(root)
+    app = App(root)
     root.mainloop()
 
 if __name__ == "__main__":
